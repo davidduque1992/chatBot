afaa97f2646e34eb7aea4b0076e0b7214e804bd0
davidduque1992 <105513600+davidduque1992@users.noreply.github.com>
2022-11-10 01:19:26 -0300

    envio de mesaje con api

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
-const qrcode = require('qrcode-terminal');
+const qrcode = require("qrcode-terminal");
 
-const { Client } = require('whatsapp-web.js');
+const { Client } = require("whatsapp-web.js");
 const client = new Client();
 
-client.on('qr', qr => {
-    qrcode.generate(qr, {small: true});
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
 
-// aqui se recibe el mensaje 
+
+// aqui se recibe el mensaje
 // client.on('message', message => {
 // 	console.log(message.body);
 // });
 
-client.on('message', message => {
-	if(message.body === 'Hola') {
-		message.reply('Hola como estas?');
-	}
-});
- 
-const resp= client.sendMessage('+541150398806'+'@.us','prueba de mensaje  automatico')
-console.log("enviado: ", resp);
+//aqui recibe el mensaje y devuelve el otro mensaje con reply
+// client.on('message', message => {
+// 	if(message.body === 'Hola') {
+// 		message.reply('Hola desde el bot');
+// 	}
+// });
+
+client.on('ready', () => {
+	console.log('Client is ready!');
+   
+	 // Number where you want to send the message.
+	const number = "+5491150398806";
+   
+	 // Your message.
+	const text = "Hola desde el script";
+   
+	 // Getting chatId from the number.
+	 // we have to delete "+" from the beginning and add "@c.us" at the end of the number.
+	const chatIdd = number.substring(1) + "@c.us";
+   
+	// Sending message.
+	client.sendMessage(chatIdd, text);
+   });
+
+// Sending message.
 client.initialize();
-
-
- 
