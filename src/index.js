97356bd4853585dc0240e9da61ab3f6f26c7f747
davidduque1992 <105513600+davidduque1992@users.noreply.github.com>
2022-11-11 00:21:24 -0300

    resuelto el tema de los inicios de session

--- src/index.js
+++ src/index.js
@@ -1,42 +1,28 @@
 const qrcode = require("qrcode-terminal");
+const fs = require("fs");
+const { Client, LegacySessionAuth, LocalAuth } = require("whatsapp-web.js");
 
-const { Client } = require("whatsapp-web.js");
-const client = new Client();
+const client = new Client({
+  authStrategy: new LocalAuth({
+    clientId: "clienteuno",
+  }),
+});
 
+client.on("authenticated", (session) => {});
+
+client.initialize();
 client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
+const send_message = ["5491150398806", "5491154986930"];
 
+client.on("ready", () => {
+  console.log("Listo");
 
-// aqui se recibe el mensaje
-// client.on('message', message => {
-// 	console.log(message.body);
-// });
-
-//aqui recibe el mensaje y devuelve el otro mensaje con reply
-// client.on('message', message => {
-// 	if(message.body === 'Hola') {
-// 		message.reply('Hola desde el bot');
-// 	}
-// });
-
-client.on('ready', () => {
-	console.log('Client is ready!');
-   
-	 // Number where you want to send the message.
-	const number = "+5491150398806";
-   
-	 // Your message.
-	const text = "Hola desde el script";
-   
-	 // Getting chatId from the number.
-	 // we have to delete "+" from the beginning and add "@c.us" at the end of the number.
-	const chatIdd = number.substring(1) + "@c.us";
-   
-	// Sending message.
-	client.sendMessage(chatIdd, text);
-   });
-
-// Sending message.
-client.initialize();
+  send_message.map((value) => {
+    const chatId = value + "@c.us";
+    message = "Prueba  de envio ";
+    client.sendMessage(chatId, message);
+  });
+});
